e7c3f6566522ec696bbb0b51f4bc3c21eb5b6141
donnyfauzi <donnyfauzi55@gmail.com>
2025-03-06 08:59:33 +0700

    update

--- midelwares/jwtToken.js
+++ midelwares/jwtToken.js
@@ -1,25 +1,26 @@
-const jwt = require("jsonwebtoken")
+const jwt = require('jsonwebtoken')
 
 const verifyToken = (req, res, next) => {
-  const authHeader = req.header("Authorization")
+  const authHeader = req.header('Authorization')
 
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
     return res
       .status(401)
-      .json({ message: "Akses ditolak, token tidak ditemukan" })
+      .json({ message: 'Akses ditolak, token tidak ditemukan' })
   }
 
-  const token = authHeader.split(" ")[1]; // Ambil token setelah "Bearer "
+  const token = authHeader.split(' ')[1]; // Ambil token setelah 'Bearer '
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
     req.user = decoded;
-    next();
+    next()
   } catch (error) {
-    return res.status(401).json({ message: "Token tidak valid" })
+    if (error.name === 'TokenExpiredError') {
+      return res.status(401).json({ message: 'Sesi anda telah habis, silakan login kembali' })
+    }
+    return res.status(401).json({ message: 'Token tidak valid' })
   }
-};
-
-
+}
 
 module.exports = verifyToken
